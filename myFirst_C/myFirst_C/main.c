917242c34354850806d7712017d7d69ed38df9aa
Tim <tim@Air-de-Tim-2.lan>
2024-03-07 21:39:12 +0100

    first lesson

---
+++ myFirst_C/myFirst_C/main.c
@@ -0,0 +1,43 @@
+//
+//  main.c
+//  myFirst_C
+//
+//  Created by Tim on 07/03/2024.
+//
+/*
+Ci-dessous, ce sont des directives de préprocesseur.
+Ces lignes permettent d'ajouter des fichiers au projet,
+fichiers que l'on appelle bibliothèques.
+Grâce à ces bibliothèques, on disposera de fonctions toutes prêtes pour afficher
+par exemple un message à l'écran.
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+Ci-dessous, ce sont des directives de préprocesseur.
+Ces lignes permettent d'ajouter des fichiers au projet,
+fichiers que l'on appelle bibliothèques.
+Grâce à ces bibliothèques, on disposera de fonctions toutes prêtes pour afficher
+par exemple un message à l'écran.
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+Ci-dessous, vous avez la fonction principale du programme, appelée main.
+C'est par cette fonction que tous les programmes commencent.
+Ici, ma fonction se contente d'afficher la question de Larmina .
+*/
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("\t Comment est votre\n blanquette?\n"); // Elle est bonne
+    /*
+     Larmina : On me dit le plus grand bien des harengs pomme-à-l’huile.
+     OSS 117 : Le patron vous en apportera un ramequin, vous vous ferez une idée.
+     */
+    return 0;
+}
